9babc2805d7884315f7fea7e4b73038408e4e5b4
Putra Rama Setiawan <putra.rama@digitaltelkom.id>
2026-08-14 21:22:00 +0700

    Initial Commit

---
+++ Kuma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kuma
+//
+//  Created by Putra Rama on 14/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kuma/KumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KumaApp.swift
+//  Kuma
+//
+//  Created by Putra Rama on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct KumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
